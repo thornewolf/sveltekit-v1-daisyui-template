46cbd5201d2c545d97a4a9d7c1bfd2a2c4d6fcd0
Thorne Wolf <thornecanyon@gmail.com>
2023-01-05 13:16:20 -0800

    initial commits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<h1 class="px-4 text-3xl font-bold text-white underline">Hello world!</h1>
+<div class="flex h-screen w-screen gap-2 p-2">
+	<div class="mx-auto my-auto flex flex-col place-items-center gap-2">
+		<h1 class="mx-auto px-4 text-3xl font-bold">New Project</h1>
+		<button class="btn-primary btn mx-auto max-w-lg">Begin</button>
+	</div>
+</div>
